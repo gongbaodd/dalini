5eb7d375384e16aaad55058224725a0ce56fac72
gongbaodd@Inspiron_PS <gongbaodd@qq.com>
2021-04-29 22:59:42 +0800

    ✨ Feat: show map

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,12 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import { Map } from "react-bmapgl";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <div className="App" id="App">
+      <Map center={{ lng: 121.605868, lat: 39.004631 }} zoom="11" enableScrollWheelZoom={true} enableDoubleClickZoom={true} style={{ height: "100%" }}></Map>
+    </div >
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+require('dotenv').config()
+
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { minifyHtml, injectHtml } from 'vite-plugin-html';
+
+if (!process.env.BAIDU_MAP_KEY) {
+  throw new Error("BAIDU_MAP_KEY is not set!");
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(),
+  injectHtml({
+    injectData: {
+      BAIDU_MAP_SCRIPT: `<script type="text/javascript" src="https://api.map.baidu.com/api?v=1.0&&type=webgl&ak=${process.env.BAIDU_MAP_KEY}"></script>`
+    },
+  }),
+  minifyHtml(),]
 })
