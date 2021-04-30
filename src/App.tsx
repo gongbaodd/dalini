427cb370fb15c669192c9915357b41d40c65df62
gongbaodd@Inspiron_PS <gongbaodd@qq.com>
2021-04-30 11:31:02 +0800

    ✨ Feat: Add workflow

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App" id="App">
-      <Map center={{ lng: 121.605868, lat: 39.004631 }} zoom="11" enableScrollWheelZoom={true} enableDoubleClickZoom={true} style={{ height: "100%" }}></Map>
+      <Map center={{ lng: 121.605868, lat: 39.004631 }} zoom={11} enableScrollWheelZoom={true} enableDoubleClickZoom={true} style={{ height: "100%" }}></Map>
     </div >
   )
 }
